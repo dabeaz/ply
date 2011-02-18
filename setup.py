b680111af209325e7707b803025f5a382715719d
David Beazley <dave@dabeaz.com>
2011-02-17 21:31:29 -0600

    Python3.2 fixes

--- setup.py
+++ setup.py
@@ -14,13 +14,18 @@ PLY provides most of the standard lex/yacc features including support for empty
 productions, precedence rules, error recovery, and support for ambiguous grammars. 
 
 PLY is extremely easy to use and provides very extensive error checking. 
+It is compatible with both Python 2 and Python 3.
 """,
             license="""BSD""",
-            version = "3.3",
+            version = "3.4",
             author = "David Beazley",
             author_email = "dave@dabeaz.com",
             maintainer = "David Beazley",
             maintainer_email = "dave@dabeaz.com",
             url = "http://www.dabeaz.com/ply/",
             packages = ['ply'],
+            classifiers = [
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 2',
+              ]
             )
