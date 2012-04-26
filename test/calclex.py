708ff46356912aa05d8e30a5b84d837d7a65d3b2
David Beazley <dave@dabeaz.com>
2012-04-26 10:15:09 -0500

    Fixed lexer line tracking.

--- test/calclex.py
+++ test/calclex.py
@@ -36,7 +36,7 @@ t_ignore = " \t"
 
 def t_newline(t):
     r'\n+'
-    t.lineno += t.value.count("\n")
+    t.lexer.lineno += t.value.count("\n")
     
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
