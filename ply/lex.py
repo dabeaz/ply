6be29b5e781b722319e3bdf9bbb3310a52f9ea62
David Beazley <dave@dabeaz.com>
2018-12-22 15:41:08 -0600

    Bump copyright. Slight travis change

--- ply/lex.py
+++ ply/lex.py
@@ -1,9 +1,11 @@
 # -----------------------------------------------------------------------------
 # ply: lex.py
 #
-# Copyright (C) 2001-2018
+# Copyright (C) 2001-2019
 # David M. Beazley (Dabeaz LLC)
 # All rights reserved.
+#
+# Latest version: https://github.com/dabeaz/ply# Latest version: https://github.com/dabeaz/ply
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
